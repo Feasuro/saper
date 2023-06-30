28710dbdce91b057c3c58f09c6ed5452f5cff0d6
Feasuro <feasuro@protonmail.com>
2023-06-30 13:52:57 +0200

    win/loose conditions

--- saper.py
+++ saper.py
@@ -9,11 +9,11 @@ from PyQt6.QtGui import QIcon, QPixmap, QAction
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QGridLayout, QLabel, QToolBar
 
 class Button(QPushButton):
-    """Button that covers field"""
+    """ Button that covers field """
     click = Signal(tuple)
 
     def __init__(self, field, *args, **kwargs):
-        """button is aware of it's position that is emitted when clicked"""
+        """ button is aware of it's position that is emitted when clicked """
         super().__init__(*args, **kwargs)
         self.setProperty('field', field)
         self.clicked.connect(self.on_click)
@@ -24,32 +24,33 @@ class Button(QPushButton):
 
 
 class Board(QWidget):
-    """Widget that represents the game board"""
+    """ Widget that represents the game board """
+    lost = Signal()
+    won = Signal()
     
     def __init__(self, x, y, bombcount, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
+        self.bombcount = bombcount
         self.wincounter = 0
+        #labels that store field contents
         self.field = {(i,j): QLabel() for i in range(x) for j in range(y)}
         for f in self.field:
             self.field[f].setProperty('covered', True)
             self.wincounter += 1
-        self.populate(bombcount)
-        
+        self.populate()
+        #make layout and fill with covering buttons
         self.setFixedSize(18 * x, 18 * y)
         self.layout = QGridLayout()
         self.layout.setSpacing(0)
         for f in self.field:
-            #self.field[f].setFixedSize(QSize(18, 18))
+            self.field[f].setFixedSize(QSize(18, 18))
             self.layout.addWidget(Button(f, click=self.uncover), f[0], f[1])
-            #self.layout.addWidget(self.field[*f], *f)
-            #sygnałki
-            #self.layout.itemAtPosition(*f).widget().clicked.connect(self.on_click)
         self.setLayout(self.layout)
     
-    def populate(self, bombcount):
-        """Fills board with mines and numbers"""
-        self.bombs = random.sample(sorted(self.field), bombcount)
+    def populate(self) -> None:
+        """ Fills board with mines and numbers """
+        self.bombs = random.sample(sorted(self.field), self.bombcount)
         self.empty = []
         for f in self.bombs:
             self.field[f].setPixmap(QPixmap('./resources/mine.png').scaled(18, 18))
@@ -64,10 +65,10 @@ class Board(QWidget):
                 if counter == 0:
                     self.empty.append(f)
                 else:
-                    self.field[f].setText(str(counter)) #poprawić kolory
+                    self.field[f].setText(str(counter))
     
     def neighborhood(self, f: tuple) -> list:
-        """Returns neighbor fields to the given one"""
+        """ Returns neighbor fields to the given one """
         neighbors = []
         for i in range(f[0]-1, f[0]+2):
             for j in range(f[1]-1, f[1]+2):
@@ -78,33 +79,45 @@ class Board(QWidget):
         return neighbors
     
     def uncover(self, field) -> bool:
-        """Method reveals content of the field(s)"""
+        """ Method reveals content of the field(s) """
         if not self.field[field].property('covered'):
             return False
-        #print('click!', field)
+
         button = self.layout.itemAtPosition(*field).widget()
         self.layout.removeWidget(button)
-        #button.deleteLater()
-        #del button
-        #self.layout.replaceWidget(old, self.field[field])
         self.layout.addWidget(self.field[field], *field)
         self.field[field].setProperty('covered', False)
-        
+        #recurrent uncovering
         if field in self.empty:
             for f in self.neighborhood(field):
                 self.uncover(f)
-        
+        #loose when you click a bomb
         if field in self.bombs:
-            #print('You Lost!') #to be continued
-            for f in self.bombs:
-                self.uncover(f)
-                #self.layout.itemAtPosition(*f).widget().click
-        self.wincounter -= 1
+            self.failure()
+        #check victory condition
+        self.victory()
         return True
+    
+    def failure(self) -> None:
+        """ Show bombs, deactivate fields, and send lost signal """
+        for f in self.bombs:
+                self.uncover(f)
+        for f in self.field:
+            if self.field[f].property('covered') == True:
+                self.layout.itemAtPosition(*f).widget().setFlat(True)
+        self.lost.emit()
+    
+    def victory(self) -> None:
+        """ Decrease counter, check condition, deactivate bomb-fields and send win signal """
+        self.wincounter -= 1
+        if self.wincounter == self.bombcount :
+            for f in self.bombs:
+                self.layout.itemAtPosition(*f).widget().setFlat(True)
+            self.won.emit()
 
 
 class MainWindow(QMainWindow):
-    """Provides graphic interface for playing saper"""
+    """ Provides window interface for playing saper """
     
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
@@ -112,28 +125,49 @@ class MainWindow(QMainWindow):
         #size and title bar
         self.setWindowTitle('Saper')
         self.setWindowIcon(QIcon('./resources/mine.png'))
-        #self.setGeometry(100,100,700,500)
         
-        new = QAction(QIcon('./resources/smiley.png'), '&New' , self)
-        new.triggered.connect(self.new_game)
-        
+        self.ui_setup()
+        self.new_game()
+    
+    def ui_setup(self):
+        """ Arranges all window elements. """
+        #icons for central button
+        self.smiley = QIcon('./resources/smiley.png')
+        self.sad = QIcon('./resources/sad.png')
+        self.wow = QIcon('./resources/wow.png')
+        #action of central button
+        self.new = QAction(self.smiley, '&New' , self)
+        self.new.triggered.connect(self.new_game)
+        #toolbar
         toolbar = QToolBar()
         toolbar.setIconSize(QSize(32, 32))
         toolbar.setMovable(False)
-        toolbar.addAction(new)
+        toolbar.addAction(self.new)
         self.addToolBar(toolbar)
-        
+        #statusbar
         self.statusbar = self.statusBar()
         self.statuslabel = QLabel('Bombs left')
         self.timer = QLabel('Time:')
         self.statusbar.addPermanentWidget(self.statuslabel)
         self.statusbar.addPermanentWidget(self.timer)
-        
-        self.new_game()
     
     def new_game(self):
+        """ Set up for a new game """
+        self.new.setIcon(self.smiley)
+        self.statuslabel.setText('Bombs left')
         self.playground = Board(10, 10, 10)
+        self.playground.lost.connect(self.handle_failure)
+        self.playground.won.connect(self.handle_victory)
         self.setCentralWidget(self.playground)
+    
+    def handle_failure(self):
+        """ Communicate failure to the player """
+        self.statuslabel.setText('You lost!')
+        self.new.setIcon(self.sad)
+
+    def handle_victory(self):
+        """ Communicate victory to the player """
+        self.statuslabel.setText('Victory!')
 
 
 if __name__ == '__main__':
